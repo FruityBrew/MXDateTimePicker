0ae719bbe76a356043d488d2a37e749d16665e85
Александр Сергеевич Ковалев <kompota@yandex.ru>
2017-01-26 16:06:54 +0300

    #DateTimePicker Поправлено в generic

--- DateTimePicker/DateTimePicker.cs
+++ DateTimePicker/DateTimePicker.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace DateTimePicker
 {
-    public class DateTimePicker
+    public class DateTimePicker : Control
     {
     }
 }
