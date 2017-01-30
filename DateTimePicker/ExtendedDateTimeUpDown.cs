ee74e731117d8961fd392d39baaebab8fbf5c6ed
Александр Сергеевич Ковалев <kompota@yandex.ru>
2017-01-30 09:58:43 +0300

    #MXDatePicker изучение контролов

---
+++ DateTimePicker/ExtendedDateTimeUpDown.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using Xceed.Wpf.Toolkit;
+
+namespace DateTimePicker
+{
+    [TemplatePart(Name = PART_TimeUpDown, Type = typeof(DateTimeUpDown))]
+    public class ExtendedDateTimeUpDown : DateTimeUpDown
+    {
+        #region Constants
+
+        private const string PART_TimeUpDown = "PART_TimeUpDown";
+
+        #endregion Constants
+
+        #region Static
+
+        #region Constructors
+
+        static ExtendedDateTimeUpDown()
+        {
+            // предоставление нового стиля:
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(ExtendedDateTimeUpDown),
+                new FrameworkPropertyMetadata(typeof(ExtendedDateTimeUpDown)));
+        }
+        #endregion Constructors
+
+
+        #endregion Static
+
+        #region Fields
+
+        private DateTimeUpDown _dateTimeUpDown;
+
+        private Timer _timeUpdater;
+
+        private DateTime _currentDateTime;
+
+        #endregion Fields
+
+        #region Properties
+        
+        public DateTime CurrentDateTime
+        {
+            get
+            {
+                return this._currentDateTime;
+            }
+            set
+            {
+                this._currentDateTime = value;
+            }
+        }
+
+        #endregion Properties
+
+        #region Constructors
+        public ExtendedDateTimeUpDown()
+        {
+            this._currentDateTime = DateTime.Now;
+            this._timeUpdater = new Timer(1000);
+            this._timeUpdater.Elapsed += this._OnTimeEvent;
+            this._timeUpdater.Start();
+        }
+
+
+        #endregion Construcnors
+
+        #region Utilities
+        #endregion Utilities
+
+        #region Methods
+        #endregion Methods
+
+        #region Methods overrides
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+
+        }
+
+        #endregion Methods overrides
+
+
+        #region Event handlers
+        private void _OnTimeEvent(Object source, ElapsedEventArgs arg)
+        {
+            this.CurrentDateTime = DateTime.Now;
+        }
+        #endregion Event handlers
+
+        #region Events
+        #endregion Events
+    }
+}
---
+++ TestExtendedWPFToolkit/ViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TestExtendedWPFToolkit
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private DateTime _dateTime;
+
+        private Timer _timer;
+
+        public DateTime DT 
+        {
+            get
+            {
+                return this._dateTime;
+            }
+            set
+            {
+                this._dateTime = value;
+                this.OnPropertyChanged("DT");
+            }
+        }
+
+        public ViewModel()
+        {
+            this._dateTime = DateTime.Now;
+            this._timer = new Timer(1000);
+            this._timer.Elapsed += this._OnTimeEvent;
+            this._timer.Start();
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(
+                    this,
+                    new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void _OnTimeEvent(Object source, ElapsedEventArgs arg)
+        {
+            this.DT = DateTime.Now;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
