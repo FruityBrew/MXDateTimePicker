13394d9e3655e6fe2dff73b74b08a736db63b327
Александр Сергеевич Ковалев <kompota@yandex.ru>
2017-01-30 10:47:46 +0300

    #MXDateTimePicker Добавлено наследование от DateTimeUpDown Добавлен DefaultStyle из сорса ExtendedWPFToolKit

--- DateTimePicker/DateTimePicker.cs
+++ DateTimePicker/DateTimePicker.cs
@@ -3,11 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
 using System.Windows.Controls;
+using Xceed.Wpf.Toolkit;
 
 namespace DateTimePicker
 {
-    public class DateTimePicker : Control
+    public class DateTimePicker : DateTimeUpDown
     {
+         #region Constants
+
+        //private const string PART_TimeUpDown = "PART_TimeUpDown";
+
+        #endregion Constants
+
+        #region Static
+
+        #region Constructors
+
+        static DateTimePicker()
+        {
+            // предоставление стиля по умолчанию:
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(DateTimePicker),
+                new FrameworkPropertyMetadata(typeof(DateTimePicker)));
+        }
+        #endregion Constructors
+
+
+        #endregion Static
+
+        #region Fields
+
+        //private DateTimeUpDown _dateTimeUpDown;
+
+        private Timer _timeUpdater;
+
+        private DateTime _currentDateTime;
+
+        #endregion Fields
+
+        #region Properties
+        
+        public DateTime CurrentDateTime
+        {
+            get
+            {
+                return this._currentDateTime;
+            }
+            set
+            {
+                this._currentDateTime = value;
+            }
+        }
+
+        #endregion Properties
+
+        #region Constructors
+        public DateTimePicker()
+        {
+            this._currentDateTime = DateTime.Now;
+            this._timeUpdater = new Timer(1000);
+            this._timeUpdater.Elapsed += this._OnTimeEvent;
+            this._timeUpdater.Start();
+        }
+
+
+        #endregion Construcnors
+
+        #region Utilities
+        #endregion Utilities
+
+        #region Methods
+        #endregion Methods
+
+        #region Methods overrides
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+
+        }
+
+        #endregion Methods overrides
+
+
+        #region Event handlers
+        private void _OnTimeEvent(Object source, ElapsedEventArgs arg)
+        {
+            this.CurrentDateTime = DateTime.Now;
+        }
+        #endregion Event handlers
+
+        #region Events
+        #endregion Events
     }
 }
